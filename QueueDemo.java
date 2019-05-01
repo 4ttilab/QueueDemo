c9ca8f59b774ccc40c9cb3a7c8202f92ee876e81
Attila B <4ttilab@gmail.com>
2019-05-01 13:13:03 +0200

    Add files via upload

---
+++ QueueDemo.java
@@ -0,0 +1,33 @@
+package queuedemo;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class QueueDemo {
+
+    public static void main(String[] args) {
+        Queue<String> qu = new PriorityQueue<>(); 
+        qu.add("B Gizi");             //így tudok hozzáadni elemet a sorhoz
+        System.out.println(qu);
+        qu.add("C Gyula");
+        System.out.println(qu);
+        qu.add("A Frodó");              //így is hozzá tudok adni elemet a sorhoz
+        System.out.println(qu);
+        System.out.println(qu.element());  //megnézhetem, ki az első a sorban
+        qu.poll();                      //az elsőt kiveszem
+        System.out.println(qu);
+        qu.offer("D Csókos Jani"); 
+        System.out.println(qu);
+        System.out.println(qu.peek());
+        qu.add("B Béla"); 
+        System.out.println(qu);
+        System.out.println(qu.peek());
+        qu.poll();
+        System.out.println(qu);
+        qu.poll();
+        System.out.println(qu);
+        qu.poll();
+        System.out.println(qu); //
+        qu.poll();
+        System.out.println(qu);
+    }
+}
